163ed04afc81b8c1bb416c1f037de5bf13f98c32
SossioG <sosiosos2002@gmail.com>
2023-01-31 10:53:10 +0100

    Pwn togs bort från gitignore

---
+++ src/main/java/se/myhappyplants/server/PasswordsAndKeys.java
@@ -0,0 +1,14 @@
+package se.myhappyplants.server;
+
+//THIS FILE IS HIDDEN FROM GIT
+public class PasswordsAndKeys {
+    //DB
+    public static String dbServerIp = "Myhappyplants.database.windows.net";
+    public static String dbServerPort = "1433";
+    public static String dbUsername = "mhpadmin";
+    public static String dbPassword = "Viälskarsven123";
+    public static String dbHostName = "x";
+
+    //API
+    public static String APIToken = "eI01vwK-LgBiMpuVI3tqDaT7xKSEyoEl2qf20rwxb9k&q";
+}
