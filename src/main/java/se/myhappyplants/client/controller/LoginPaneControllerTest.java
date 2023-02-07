919dd88b03b3f97842f1127e4072074d7e762378
Danny Gazic <dannygazic@gmail.com>
2023-02-07 10:40:13 +0100

    working test

---
+++ src/main/java/se/myhappyplants/client/controller/LoginPaneControllerTest.java
@@ -0,0 +1,14 @@
+package se.myhappyplants.client.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoginPaneControllerTest
+{
+
+    @Test
+    void test()
+    {
+        Assertions.assertEquals(2,2);
+    }
+}
