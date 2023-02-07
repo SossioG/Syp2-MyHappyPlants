f7299f4e07457137a17152b0d472c2eebc8541df
ElisabetAronsson <96416333+ElisabetAronsson@users.noreply.github.com>
2023-02-07 10:29:08 +0100

    Ska gå ut och in projektet igen för att se om modifieringen av POM filen orsakade att testfilen inte fungerar som den ska

---
+++ src/test/java/se/myhappyplants/client/model/RegistrationTest.java
@@ -0,0 +1,15 @@
+package se.myhappyplants.client.model;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RegistrationTest {
+
+    /**
+     * Req: ANV12F
+     */
+
+    @Test
+    void thisisatest{
+        Assertions.assertEquals(1,1);
+    }
+}
