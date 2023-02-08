72a7d9addf0115d0e17b6a6f0bab16e2c4062f4a
ElisabetAronsson <96416333+ElisabetAronsson@users.noreply.github.com>
2023-02-08 15:24:46 +0100

    Skapade två testfiler med tillhörande testkod, ANV03F med RegisterPaneControllerMock och BIB01F med PlantLibraryMock.

---
+++ src/test/java/mockcodefiles/PlantLibraryMock.java
@@ -0,0 +1,47 @@
+package mockcodefiles;
+
+import se.myhappyplants.shared.Plant;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class PlantLibraryMock {
+    private ArrayList<Plant> currentUserLibrary;
+
+    public PlantLibraryMock(){
+        currentUserLibrary = new ArrayList<>();
+        long currentDateMilli = System.currentTimeMillis();
+        Date date = new Date(currentDateMilli);
+        currentUserLibrary.add(new Plant("Bärta", "1",date));
+        currentUserLibrary.add(new Plant("Märta", "2",date));
+    }
+
+    public boolean addPlantToCurrentUserLibrary(Plant plantAdd, boolean wantToAdd) {
+        String plantNickname = "commonName";
+
+        //int answer = MessageBox.askYesNo(BoxTitle.Add, "Do you want to add a nickname for your plant?");
+        if (wantToAdd == true) {
+            plantNickname = "MinFavoritPlanta";//MessageBox.askForStringInput("Add a nickname", "Nickname:");
+        }
+        return addPlantToCurrentUserLibraryPanel(plantAdd, plantNickname);
+    }
+
+    public boolean addPlantToCurrentUserLibraryPanel(Plant selectedPlant, String plantNickname) {
+        int plantsWithThisNickname = 1;
+        String uniqueNickName = plantNickname;
+        for (Plant plant : currentUserLibrary) {
+            if (plant.getNickname().equals(uniqueNickName)) {
+                plantsWithThisNickname++;
+                uniqueNickName = plantNickname + plantsWithThisNickname;
+            }
+        }
+        long currentDateMilli = System.currentTimeMillis();
+        Date date = new Date(currentDateMilli);
+        //String imageURL = PictureRandomizer.getRandomPictureURL();
+        Plant plantToAdd = new Plant(uniqueNickName, selectedPlant.getPlantId(), date);
+        currentUserLibrary.add(plantToAdd);
+        //PopupBox.display(MessageText.sucessfullyAddPlant.toString());
+        return true;
+        //addPlantToDB(plantToAdd);
+    }
+}
--- src/test/java/se/myhappyplants/client/model/RegistrationTest.java
+++ src/test/java/se/myhappyplants/client/model/RegistrationTest.java
@@ -1,15 +1,62 @@
 package se.myhappyplants.client.model;
+
+import mockcodefiles.RegisterPaneControllerMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class RegistrationTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+class RegistrationTest {
     /**
-     * Req: ANV12F
+     * Reqid: ANV03F
+     * Skapa konto - MUST  - En användare ska kunna skapa ett nytt konto genom att ange e-postadress, namn och lösenord.
      */
+    private final String validEmail = "firstname.surname@gmail.com";
+    private final String validUsername = "username123";
+    private final String validPassword = "Password1";
+    private
+    Verifier verifier = new Verifier();
+
 
     @Test
-    void thisisatest{
-        Assertions.assertEquals(1,1);
+    void validReg(){
+        RegisterPaneControllerMock mock = new RegisterPaneControllerMock(validEmail, validEmail,
+                validUsername, validPassword, validPassword);
+        Assertions.assertTrue(mock.validateRegistration(mock));
+    }
+
+    @Test
+    void emailCheckWhenEmptyStrings(){
+        RegisterPaneControllerMock mock = new RegisterPaneControllerMock("", "",
+                validUsername, validPassword, validPassword);
+        Assertions.assertFalse(mock.validateRegistration(mock));
+    }
+
+    @Test
+    void usernameEmptyStrings(){
+        RegisterPaneControllerMock mock = new RegisterPaneControllerMock(validEmail, validEmail, "",
+                validPassword, validPassword);
+        Assertions.assertFalse(mock.validateRegistration(mock));
+    }
+
+    @Test
+    void passwordFirstEmptyStrings(){
+        RegisterPaneControllerMock mock = new RegisterPaneControllerMock(validEmail, validEmail, validUsername,
+                "", validPassword);
+        Assertions.assertFalse(mock.validateRegistration(mock));
+    }
+
+    @Test
+    void passwordSecondEmptyStrings(){
+        RegisterPaneControllerMock mock = new RegisterPaneControllerMock(validEmail, validEmail, validUsername,
+                validPassword, "");
+        Assertions.assertFalse(mock.validateRegistration(mock));
+    }
+
+    @Test
+    void passwordDontMatch(){
+        RegisterPaneControllerMock mock = new RegisterPaneControllerMock(validEmail, validEmail, validUsername,
+                validPassword, validPassword + "a");
+        Assertions.assertFalse(mock.validateRegistration(mock));
     }
 }
