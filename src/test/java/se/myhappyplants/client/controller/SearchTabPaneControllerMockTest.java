389724c89e4fdcd510674a29d51d62d401140637
Danny Gazic <dannygazic@gmail.com>
2023-02-11 13:05:32 +0100

    added descriptive text in test class

--- src/test/java/se/myhappyplants/client/controller/SearchTabPaneControllerMockTest.java
+++ src/test/java/se/myhappyplants/client/controller/SearchTabPaneControllerMockTest.java
@@ -3,16 +3,20 @@ package se.myhappyplants.client.controller;
 import mockcodefiles.SearchTabPaneControllerMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.net.UnknownHostException;
 import java.sql.SQLException;
+
+/**
+ * ReqID: SÖK01F
+ * En användare ska kunna söka på olika växter.
+ */
 
 class SearchTabPaneControllerMockTest {
 
     SearchTabPaneControllerMock searchTabMock = new SearchTabPaneControllerMock();
 
     @Test
-    void test() throws UnknownHostException, SQLException {
+    void testSearchForPlant() throws UnknownHostException, SQLException {
         searchTabMock.setSearchText("TestPlanta");
         Assertions.assertTrue(searchTabMock.searchButtonPressed());
     }
