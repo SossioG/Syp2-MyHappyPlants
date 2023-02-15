b76bb6c08a2ff362a190eb6df102cb3183eb6cd5
Danny Gazic <dannygazic@gmail.com>
2023-02-15 14:55:20 +0100

    almost done mail class

--- src/main/java/se/myhappyplants/shared/Email.java
+++ src/main/java/se/myhappyplants/shared/Email.java
@@ -1,39 +1,72 @@
 package se.myhappyplants.shared;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
 import java.util.Properties;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 
 public class Email
 {
-    /**
-     * Create a MimeMessage using the parameters provided.
-     *
-     * @param toEmailAddress   email address of the receiver
-     * @param fromEmailAddress email address of the sender, the mailbox account
-     * @param subject          subject of the email
-     * @param bodyText         body text of the email
-     * @return the MimeMessage to be used to send email
-     * @throws MessagingException - if a wrongly formatted address is encountered.
-     */
+
+    //example
+       /*
+      try {
+    MimeMessage msg = new MimeMessage(session);
+    msg.setFrom(new InternetAddress("from@example.com"));
+    InternetAddress[] address = {new InternetAddress("to@example.com")};
+    msg.setRecipients(Message.RecipientType.TO, address);
+    msg.setSubject("Jakarta Mail APIs Test");
+    msg.addHeader("x-cloudmta-class", "standard");
+    msg.addHeader("x-cloudmta-tags", "demo, example");
+    msg.setText("Test Message Content");
+
+    Transport.send(msg);
+
+    System.out.println("Message Sent.");
+} catch (javax.mail.MessagingException ex) {
+    throw new RuntimeException(ex);
+}
+      */
+
+
+    public static void main(String[]args) throws MessagingException {
+        Email.createEmail("dannygazic@gmail.com","Evout1935@einrot.com","notificationtest","testing");
+    }
+
     public static MimeMessage createEmail(String toEmailAddress,
                                           String fromEmailAddress,
                                           String subject,
                                           String bodyText)
             throws MessagingException {
         Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        jakarta.mail.Authenticator auth = new jakarta.mail.Authenticator() {
+            protected jakarta.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("kaffekopp1", "Kaffekopp1");
+            }
+        };
+
+        Session session = Session.getInstance(props, auth);
 
         MimeMessage email = new MimeMessage(session);
 
         email.setFrom(new InternetAddress(fromEmailAddress)); //input the applications mail
-        email.addRecipient(javax.mail.Message.RecipientType.TO,
+        email.addRecipient(jakarta.mail.Message.RecipientType.TO,
                 new InternetAddress(toEmailAddress));   //the user to send mail to
         email.setSubject(subject); //subject = notification
-        email.setText(bodyText); //what is the notification mail should say
+        email.setText(bodyText); //what the notification mail should say
+        Transport.send(email); //send the mail
         return email;
     }
+
+
 }
