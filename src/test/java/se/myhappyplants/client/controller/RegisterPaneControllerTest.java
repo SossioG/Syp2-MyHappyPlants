975dfe90fae6ef4840f16ea8e5e6ec0550d68cef
ecspresso <emile@ecspreso.com>
2023-02-21 11:05:32 +0100

    RegistrationPane tester klara & förbättringar i robotorna.
    Alla tester klara för registrationpane.
    Tog bort mockklassen för registration pane.
    KeyRobot har förbättrad fördröjning på musen.
    KeyRobot kan nu sätta texten direkt i TextField istället för att klicka
    och skriva in texten, gör att tester går lite snabbare.
    DBRobot sätter  connection till null för att kunna öppna en ny om den
    skulle ha stängts i ett tidigare skede.

--- src/test/java/se/myhappyplants/client/controller/RegisterPaneControllerTest.java
+++ src/test/java/se/myhappyplants/client/controller/RegisterPaneControllerTest.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
@@ -38,9 +39,9 @@ import java.sql.SQLException;
 
 class RegisterPaneControllerTest extends ApplicationTest {
     //Create a new user in the register pane.
-    static String regEmail = "testEmail@.se";
-    static String regUsername = "testUser123";
-    static String regPassword = "testPassword123";
+    static String validEmail = "testuser@example.com";
+    static String validUsername = "TestUsername";
+    static String validPassword = "Password1";
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -56,7 +57,7 @@ class RegisterPaneControllerTest extends ApplicationTest {
 
     @BeforeEach
     void deleteUser() {
-        String query = String.format("DELETE FROM tuser WHERE username = '%s' OR email = '%s'", regUsername, regEmail);
+        String query = String.format("DELETE FROM tuser WHERE username = '%s' OR email = '%s'", validUsername, validEmail);
         try {
             DBRobot.runQuery(query);
         } catch(SQLException e) {
@@ -89,11 +90,11 @@ class RegisterPaneControllerTest extends ApplicationTest {
 
     private void enterRegFields(String email1, String email2, String username, String password1, String password2) {
         // Skriv in användaruppgifter i fälten.
-        if(email1 != null) KeyRobot.clickAndWrite("#txtFldNewEmail", email1);
-        if(email2 != null) KeyRobot.clickAndWrite("#txtFldNewEmail1", email2);
-        if(username != null) KeyRobot.clickAndWrite("#txtFldNewUsername", username);
-        if(password1 != null) KeyRobot.clickAndWrite("#passFldNewPassword", password1);
-        if(password2 != null) KeyRobot.clickAndWrite("#passFldNewPassword1", password2);
+        if(email1 != null) KeyRobot.setText("#txtFldNewEmail", email1);
+        if(email2 != null) KeyRobot.setText("#txtFldNewEmail1", email2);
+        if(username != null) KeyRobot.setText("#txtFldNewUsername", username);
+        if(password1 != null) KeyRobot.setText("#passFldNewPassword", password1);
+        if(password2 != null) KeyRobot.setText("#passFldNewPassword1", password2);
         KeyRobot.click("#signUpButton");
 
         // Vänta på att FX är klar.
@@ -101,7 +102,6 @@ class RegisterPaneControllerTest extends ApplicationTest {
     }
 
     private void assertMessageAndClick(String expected) {
-        WaitForAsyncUtils.waitForFxEvents();
         Node messageLbl = KeyRobot.getNode("#messageLbl");
         Assertions.assertThat(messageLbl).isNotNull().isInstanceOf(Label.class);
         Assertions.assertThat(((Label) messageLbl).getText()).isEqualTo(expected);
@@ -112,14 +112,31 @@ class RegisterPaneControllerTest extends ApplicationTest {
         KeyRobot.click(okButton);
     }
 
+    private void assertUserNotExists() {
+        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", validUsername);
+        try(ResultSet result = DBRobot.getQuery(query)) {
+            Assertions.assertThat(result.next()).isFalse();
+        } catch(SQLException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    private void clearTextFieldAndAssertEmpty(String query) {
+        Node node = KeyRobot.getNode(query);
+        KeyRobot.clearText(node);
+        WaitForAsyncUtils.waitForFxEvents();
+        Assertions.assertThat(((TextField) node).getText()).isEmpty();
+    }
+
     //Test där ny användare registreras med giltig indata. Testanvändaren raderas från databasen.
     @Test
     @DisplayName("Registration goes through with valid inserts")
-    public void registerNewUser() {
-        enterRegFields(regEmail, regEmail, regUsername, regPassword, regPassword);
-        assertMessageAndClick("Account created successfully! Now logged in as " + regUsername);
+    public synchronized void registerNewUser() throws InterruptedException {
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, validPassword);
+        Thread.sleep(2000);
+        assertMessageAndClick("Account created successfully! Now logged in as " + validUsername);
 
-        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", regUsername);
+        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", validUsername);
         try(ResultSet result = DBRobot.getQuery(query)) {
             result.next();
 
@@ -128,80 +145,277 @@ class RegisterPaneControllerTest extends ApplicationTest {
             String email = result.getString("email");
 
             Assertions.assertThat(id).isNotNull().isInstanceOf(Integer.class);
-            Assertions.assertThat(regUsername).isEqualTo(username);
-            Assertions.assertThat(regEmail).isEqualTo(email);
+            Assertions.assertThat(RegisterPaneControllerTest.validUsername).isEqualTo(username);
+            Assertions.assertThat(RegisterPaneControllerTest.validEmail).isEqualTo(email);
         } catch(SQLException e) {
             Assertions.fail(e.getMessage());
         }
     }
 
     @Test
-    @DisplayName("Registration does not go through with empty email insert")
-    void registerNewUserEmptyEmail() {
-        enterRegFields(null, null, regUsername, regPassword, regPassword);
+    @DisplayName("Registration does not go through with first email insert not matching second email insert")
+    void registerNewUserEmailMismatchFirst() {
+        enterRegFields("user@example.com", validEmail, validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter the same email twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through with second email insert not matching first email insert")
+    void registerNewUserEmailMismatchSecond() {
+        enterRegFields(validEmail, "user@example.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter the same email twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through with first email insert being empty")
+    void emailFirstEmptyString() {
+        enterRegFields(null, validEmail, validUsername, validPassword, validPassword);
         assertMessageAndClick("Make sure to fill all boxes.");
+        assertUserNotExists();
+    }
 
-        enterRegFields(regEmail, null, null, null, null);
+    @Test
+    @DisplayName("Registration does not go through with second email insert being empty")
+    void emailSecondEmptyString() {
+        enterRegFields(null, validEmail, validUsername, validPassword, validPassword);
         assertMessageAndClick("Make sure to fill all boxes.");
+        assertUserNotExists();
+    }
 
-        KeyRobot.clearText("#txtFldNewEmail");
-
-        enterRegFields(null, regEmail, null, null, null);
+    @Test
+    @DisplayName("Registration does not go through with both email insert being empty")
+    void emailBothEmptyString() {
+        enterRegFields(null, null, validUsername, validPassword, validPassword);
         assertMessageAndClick("Make sure to fill all boxes.");
+        assertUserNotExists();
+    }
 
 
-        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", regUsername);
-        try(ResultSet result = DBRobot.getQuery(query)) {
-            Assertions.assertThat(result.next()).isFalse();
-        } catch(SQLException e) {
-            Assertions.fail(e.getMessage());
-        }
+    // first email bad input without @
+    @Test
+    @DisplayName("Registration does not go through with first email insert missing at sign")
+    void emailFirstNoAt() {
+        enterRegFields("userexample.com", validEmail, validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
+    }
+
+    // first email bad input with @ at end
+    @Test
+    @DisplayName("Registration does not go through with first email missing domain")
+    void emailFirstNoDomain() {
+        enterRegFields("user@.com", validEmail, validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
+    }
+
+    // frist email bad input with @ at start
+    @Test
+    @DisplayName("Registration does not go through with first email missing local-part")
+    void emailFirstNoLocalPart() {
+        enterRegFields("@example.com", validEmail, validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
+    }
+
+    // second email bad input without @
+    @Test
+    @DisplayName("Registration does not go through with second email insert missing at sign")
+    void emailSecondNoAt() {
+        enterRegFields(validEmail, "userexample.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter the same email twice.");
+        assertUserNotExists();
+    }
+
+    // second email bad input with @ at end
+    @Test
+    @DisplayName("Registration does not go through with second email insert missing domain")
+    void emailSecondNoDomain() {
+        enterRegFields(validEmail, "user@.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter the same email twice.");
+        assertUserNotExists();
+    }
+
+    // second email bad input with @ at start
+    @Test
+    @DisplayName("Registration does not go through with second email insert missing local-part")
+    void emailSecondNoLocalPart() {
+        enterRegFields(validEmail, "@example.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter the same email twice.");
+        assertUserNotExists();
+    }
+
+    // first email bad input without @ and with @ at end
+    @Test
+    @DisplayName("Registration does not go through with both email inserts missing at sign")
+    void emailBothNoAt() {
+        enterRegFields("userexample.com", "userexample.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
     }
 
     @Test
-    public void invalidEmail(){
-        enterRegFields(regEmail, regEmail, "non-valid email", regPassword, regPassword);
+    @DisplayName("Registration does not go through with both email inserts missing domain")
+    void emailBothNoDomain() {
+        enterRegFields("userexample.com", "userexample.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
+    }
 
-        // Kolla i databasen om den nya användaren har lagts till.
-        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", regUsername);
-
-       try(ResultSet result = DBRobot.getQuery(query)) {
-            Assertions.assertThat(result.next()).isFalse();
-        } catch(SQLException e) {
-            Assertions.fail(e.getMessage());
-        } finally {
-            DBRobot.closeConnection();
-        }
+    // first email bad input without @ and with @ at start
+    @Test
+    @DisplayName("Registration does not go through with both email inserts missing local-part")
+    void emailBothNoLocalPart() {
+        enterRegFields("@example.com", "@example.com", validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
     }
 
     @Test
-    public void emptyEmail(){
-        enterRegFields(regEmail, regEmail, null, regPassword, regPassword);
-
-        // Kolla i databasen om den nya användaren har lagts till.
-        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", regUsername);
-
-        try(ResultSet result = DBRobot.getQuery(query)) {
-            Assertions.assertThat(result.next()).isFalse();
-        } catch(SQLException e) {
-            Assertions.fail(e.getMessage());
-        } finally {
-            DBRobot.closeConnection();
-        }
+    @DisplayName("Registration does not go through with invalid email insert")
+    void emailInvalid() {
+        validEmail = "@@@";
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, validPassword);
+        assertMessageAndClick("Please enter your email address in format: yourname@example.com");
+        assertUserNotExists();
     }
 
-    //Försöker regristrera med ogitliga mailadresser.
     @Test
-    public void invalidPassword(){
-        enterRegFields(regEmail, regEmail, null, "regPassword", "regPassword");
+    @DisplayName("Registration does not go through with empty username insert")
+    public void emptyUsername(){
+        enterRegFields(validEmail, validEmail, null, validPassword, validPassword);
+        assertMessageAndClick("Make sure to fill all boxes.");
+        assertUserNotExists();
+    }
 
-        // Kolla i databasen om den nya användaren har lagts till.
-        String query = String.format("SELECT * FROM tuser WHERE username = '%s'", regUsername);
 
-       try(ResultSet result = DBRobot.getQuery(query)) {
-            Assertions.assertThat(result.next()).isFalse();
-        } catch(SQLException e) {
-            Assertions.fail(e.getMessage());
-        }
+    @Test
+    @DisplayName("Registration does not go through with first password insert not matching second password insert")
+    void passwordMismatchFirst() {
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, "12345678Aa");
+        assertMessageAndClick("Please enter the same password twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through with second password insert not matching first password insert")
+    void passwordMismatchSecond() {
+        enterRegFields(validEmail, validEmail, validUsername, "12345678Aa", validPassword);
+        assertMessageAndClick("Please enter the same password twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through with first password insert being empty")
+    public void passwordFirstEmpty(){
+        enterRegFields(validEmail, validEmail, validUsername, null, validPassword);
+        assertMessageAndClick("Make sure to fill all boxes.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through with second password insert being empty")
+    public void passwordSecondEmpty(){
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, null);
+        assertMessageAndClick("Make sure to fill all boxes.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when first password is missing a capital letter")
+    void passwordFirstNoCapitalLetter(){
+        enterRegFields(validEmail, validEmail, validUsername, "password1", validPassword);
+        assertMessageAndClick("Password must contain one character with uppercase.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when first password is missing a number")
+    void passwordFirstNoNumber(){
+        enterRegFields(validEmail, validEmail, validUsername, "Password", validPassword);
+        assertMessageAndClick("Password must contain one digit.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when first password is missing a capital letter and number")
+    void passwordFirstNoBoth(){
+        enterRegFields(validEmail, validEmail, validUsername, "password", validPassword);
+        assertMessageAndClick("Password must contain one character with uppercase.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when second password is missing a capital letter")
+    void passwordSecondNoCapitalLetter(){
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, "password1");
+        assertMessageAndClick("Please enter the same password twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when second password is missing a number")
+    void passwordSecondNoNumber(){
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, "Password");
+        assertMessageAndClick("Please enter the same password twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when second password is missing a capital letter and number")
+    void passwordSecondNoBoth(){
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, "password");
+        assertMessageAndClick("Please enter the same password twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when first password is too short")
+    void passwordFirstTooShort(){
+        enterRegFields(validEmail, validEmail, validUsername, "P1", validPassword);
+        assertMessageAndClick("Password must be at least 8 characters.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when second password is too short")
+    void passwordSecondTooShort(){
+        enterRegFields(validEmail, validEmail, validUsername, validPassword, "P1");
+        assertMessageAndClick("Please enter the same password twice.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when both passwords missing a capital letter")
+    void passwordBothNoCapitalLetter(){
+        enterRegFields(validEmail, validEmail, validUsername, "password1", "password1");
+        assertMessageAndClick("Password must contain one character with uppercase.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when both passwords missing a number")
+    void passwordBothNoNumber(){
+        enterRegFields(validEmail, validEmail, validUsername, "Password", "Password");
+        assertMessageAndClick("Password must contain one digit.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when both passwords missing a capital letter and number")
+    void passwordBothNoBoth(){
+        enterRegFields(validEmail, validEmail, validUsername, "password", "password");
+        assertMessageAndClick("Password must contain one character with uppercase.");
+        assertUserNotExists();
+    }
+
+    @Test
+    @DisplayName("Registration does not go through when both passwords are too short")
+    void passwordBothTooShort(){
+        enterRegFields(validEmail, validEmail, validUsername, "P1", "P1");
+        assertMessageAndClick("Password must be at least 8 characters.");
+        assertUserNotExists();
     }
 }
--- src/test/java/se/myhappyplants/robots/KeyRobot.java
+++ src/test/java/se/myhappyplants/robots/KeyRobot.java
@@ -2,10 +2,13 @@ package se.myhappyplants.robots;
 
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import org.testfx.api.FxRobot;
 import org.testfx.util.WaitForAsyncUtils;
 
 import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
@@ -25,31 +28,41 @@ public class KeyRobot {
     }
 
     public static Node getNode(String query) {
+        WaitForAsyncUtils.waitForFxEvents();
         return fxRobot.lookup(query).query();
     }
 
+    private static void mouseGlide(double x, double y, int n) {
+        Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
+
+        double x1 = mouseLocation.getX();
+        double y1 = mouseLocation.getY();
+        if (n == 0) n = 1;
+        int t = 60;
+        double dx = (x - x1) / ((double) n);
+        double dy = (y - y1) / ((double) n);
+        double dt = t / ((double) n);
+
+        for (int step = 1; step <= n; step++) {
+            try {
+                Thread.sleep((int) dt);
+            } catch(InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            robot.mouseMove((int) (x1 + dx * step), (int) (y1 + dy * step));
+        }
+    }
+
     public static void click(String query) {
-        click(query, 0);
+        click(getNode(query));
     }
 
      public static void click(String query, int delay) {
-        Node node = getNode(query);
-        click(node, delay);
+        click(getNode(query), delay);
     }
 
     public static void click(Node node) {
-        click(node, 0);
-    }
-
-    public static void click(Node node, int delay) {
-        if(delay > 0) {
-            try {
-                Thread.sleep(delay);
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
         WaitForAsyncUtils.waitForFxEvents();
         Bounds bounds = fxRobot.bounds(node).query();
 
@@ -61,11 +74,24 @@ public class KeyRobot {
         robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
     }
 
+    public static void click(Node node, int delay) {
+        WaitForAsyncUtils.waitForFxEvents();
+        Bounds bounds = fxRobot.bounds(node).query();
+
+        double x = bounds.getMaxX() - bounds.getWidth() / 2;
+        double y = bounds.getMaxY() - bounds.getHeight() / 2;
+
+        mouseGlide(x, y, delay);
+        robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+    }
+
     public static void writeText(String text) {
         writeText(text, 0);
     }
 
     public static void writeText(String text, int delay) {
+        WaitForAsyncUtils.waitForFxEvents();
         if(delay > 0) {
             try {
                 Thread.sleep(delay);
@@ -85,13 +111,21 @@ public class KeyRobot {
     }
 
     public static void clickAndWrite(String query, String text) {
-        click(query);
-        writeText(text);
+        clickAndWrite(getNode(query), text);
     }
 
     public static void clickAndWrite(Node node, String text) {
         click(node);
         writeText(text);
+    }
+
+    public static void clickAndWrite(String query, String text, int delay) {
+        clickAndWrite(getNode(query), text, delay);
+    }
+
+    public static void clickAndWrite(Node node, String text, int delay) {
+        click(node, delay);
+        writeText(text, delay);
     }
 
     public static void clearText(String query) {
@@ -108,6 +142,7 @@ public class KeyRobot {
     }
 
     public static void clearText(Node node, int delay) {
+        WaitForAsyncUtils.waitForFxEvents();
         if(delay > 0) {
             try {
                 Thread.sleep(delay);
@@ -123,5 +158,20 @@ public class KeyRobot {
         robot.keyRelease(KeyEvent.VK_CONTROL);
         robot.keyPress(KeyEvent.VK_DELETE);
         robot.keyRelease(KeyEvent.VK_DELETE);
+    }
+
+    public static void setText(String query, String text) throws IllegalArgumentException {
+        Node node = getNode(query);
+
+        if(node instanceof TextField) {
+            setText((TextField) node, text);
+        } else {
+            throw new IllegalArgumentException("Node is not a TextField");
+        }
+    }
+
+    public static void setText(TextField label, String text) {
+        // WaitForAsyncUtils.waitForFxEvents();
+        label.setText(text);
     }
 }
