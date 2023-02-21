da1a223769d93cea91b27b67c41ef88f30aaf3d8
Danny Gazic <dannygazic@gmail.com>
2023-02-21 12:02:24 +0100

    added updated regex for mail verifying

--- src/main/java/se/myhappyplants/client/model/Verifier.java
+++ src/main/java/se/myhappyplants/client/model/Verifier.java
@@ -64,7 +64,7 @@ public class Verifier {
      * @return true if the email contains @, false if it is not valid
      */
     public boolean validateEmail(String email) {
-        final String regex = "^(.+)@(.+)$";
+        final String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
