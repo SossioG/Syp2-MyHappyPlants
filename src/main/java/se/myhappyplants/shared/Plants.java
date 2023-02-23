3e7ed63d884d91259692842e415ec9c8d0f897c0
ElisabetAronsson <elisabet.aronsson24@gmail.com>
2023-02-23 14:22:14 +0100

    Plants

---
+++ src/main/java/se/myhappyplants/shared/Plants.java
@@ -0,0 +1,15 @@
+package se.myhappyplants.shared;
+
+import java.util.ArrayList;
+
+public class Plants{
+    private ArrayList<Plant> plants;
+
+    public ArrayList<Plant> getPlants() {
+        return plants;
+    }
+
+    public void setPlants(ArrayList<Plant> plants) {
+        this.plants = plants;
+    }
+}
